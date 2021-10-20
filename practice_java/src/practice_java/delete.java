a991ecb6867f108202e04348fb6e74e2fe85e536
PNK25 <92137048+PNK25@users.noreply.github.com>
2021-10-20 09:33:20 +0530

    First Commit
     selenium java project

---
+++ practice_java/src/practice_java/delete.java
@@ -0,0 +1,55 @@
+package practice_java;
+
+public class delete {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  int s = 273636;
+	      float e = 7.6f;
+	      byte b = 124 ;
+	      short k = 1226;
+	      double d= 235.56;
+	      long l = 346674;
+	      char c = 'v';
+	      boolean bflag =true;
+	      
+	      System.out.println("Byte value is :" +b);
+	      System.out.println("float value is :" +e);
+	      System.out.println("short value is :" +k);
+	      System.out.println("double value is :" +d);	
+	      System.out.println("long value is :" +l);
+	      System.out.println("char value is :" +c);
+	      System.out.println("boolean value is :" +bflag);
+	      
+	      byte b2=123;
+	      int s1=45674;
+	      short f1=(short)s1;
+	      System.out.println("shot is:"  +f1);
+	      
+	      float d1= 6.4f;
+	      byte g1=(byte)d1;
+	      System.out.println("byte value is "+g1);
+	      
+	      long l1=f1;
+	      System.out.println("long value is"+l1);
+	      
+	      short r1=g1;
+	      System.out.println("shot is:"  +r1);
+	      
+	      double c1=(double)(b2+s1) ;
+	      System.out.println("double value is "  +c1);
+	      
+	      byte v1=(byte)(s1+f1+d1+g1+l1+r1+c1);
+	      System.out.println("byte value is  "   +v1);
+	      
+	      int a1=(int)(d1-g1-l1);
+	      System.out.println("int value is:  "+a1);
+	      
+	      
+	      
+	   
+	      
+	    
+	}
+
+}
