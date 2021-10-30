5773d926293f6945b4a327f420cad0845fa78230
PNK25 <92137048+PNK25@users.noreply.github.com>
2021-10-30 10:30:22 +0530

    First commit

---
+++ practice_java/src/practice_java/Remove_special_character_stringAndnumbear.java
@@ -0,0 +1,17 @@
+package practice_java;
+
+public class Remove_special_character_stringAndnumbear {
+
+	public static void main(String[] args) {
+		String s="374(233)24#$-224!56";
+		String e="fhieifjl^ieeieiee#@efe$";
+		String j=e.replaceAll("[^a-zA-Z0-9]","");
+		String h=e.replaceAll("[^a-zA-Z]","");
+		String g=s.replaceAll("[^a-zA-Z0-9]","");
+		String f=s.replaceAll("[^0-9]","");
+		System.out.println(g+"  removing special character:  "+f);
+		System.out.println(j+"   removing special character:  "+h);
+
+	}
+
+}
