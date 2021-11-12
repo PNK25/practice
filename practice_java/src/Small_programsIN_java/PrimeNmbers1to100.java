4df80a557c5caf5b5686876d4e2a6e1ee8fd769d
PNK25 <92137048+PNK25@users.noreply.github.com>
2021-11-12 11:28:22 +0530

    Today new prime number,and reverse order number

---
+++ practice_java/src/Small_programsIN_java/PrimeNmbers1to100.java
@@ -0,0 +1,14 @@
+package Small_programsIN_java;
+
+public class PrimeNmbers1to100 {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=100;i++) {
+		if(i%2==0) System.out.print(i+",");
+		
+			
+		}
+
+	}
+
+}
---
+++ practice_java/src/Small_programsIN_java/ReverseNumberFrom10To1.java
@@ -0,0 +1,13 @@
+package Small_programsIN_java;
+
+public class ReverseNumberFrom10To1 {
+
+	public static void main(String[] args) {
+		for(int i=10;i>=1;i--) {
+			
+			System.out.println(i);
+		}
+		
+	}
+
+}
