80d4beb516b7d1a977b483c552fdb13206a7fe0a
PNK25 <92137048+PNK25@users.noreply.github.com>
2021-11-28 11:18:37 +0530

    IN string each and every character  count in string

--- practice_java/src/com/roufe/Myeachcharacter.java
+++ practice_java/src/com/roufe/Myeachcharacter.java
@@ -3,25 +3,17 @@ package com.roufe;
 public class Myeachcharacter {
 	
 	public void eachcharacter(String str) {
-		
-		
-		
-	
-		
 		while(str.length()>0) {
 			 
 		char ch=str.charAt(0);
 		String h=str.replace(""+ch,"");
-		
 		int total=str.length()-h.length();
-		
 		System.out.println(ch+"  "+total);
 		str=str.replace(""+ch,"");
 		
 		}
 		
 		}
-	
 	
 	public static void main(String[] args) {
 		Myeachcharacter ch=new Myeachcharacter();
